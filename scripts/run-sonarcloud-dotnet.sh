5d1bd32e54e5052424e8b92b4396661d4c536922
anbhiman <40796080+anbhiman@users.noreply.github.com>
2024-04-19 10:36:02 +0530

    Update run-sonarcloud-dotnet.sh

--- scripts/run-sonarcloud-dotnet.sh
+++ scripts/run-sonarcloud-dotnet.sh
@@ -8,3 +8,4 @@ fi
 ./.sonar/scanner/dotnet-sonarscanner begin /k:"$PROJECT_KEY" /o:"$ORGANIZATION" /d:sonar.login="$SONAR_TOKEN" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.c.file.suffixes=-
 dotnet build
 ./.sonar/scanner/dotnet-sonarscanner end /d:sonar.login="$SONAR_TOKEN"
+
